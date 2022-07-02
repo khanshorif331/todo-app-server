5512342ea4630c5b1b42f84728f2de867bd3d0fb
khanshorif331 <khanshorif331@gmail.com>
2022-07-02 12:44:59 +0600

    created route for adding completed tasks in the db

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run() {
 		console.log('Database Connected')
 		const todoCollection = client.db('todo-app').collection('todos')
 		const userCollection = client.db('todo-app').collection('users')
+		const completeCollection = client.db('todo-app').collection('completed')
 
 		// finding todo items according email
 		app.get('/todos/:email', async (req, res) => {
@@ -49,6 +50,35 @@ async function run() {
 			res.send(result)
 		})
 
+		// completed task status
+		app.put('/todo/:id', async (req, res) => {
+			const id = req.params.id
+			const filter = { _id: ObjectId(id) }
+			const updateDoc = {
+				$set: { status: 'completed' },
+			}
+			const result = await todoCollection.updateOne(filter, updateDoc)
+			res.send(result)
+		})
+
+		// completed todolist posting to the db
+		app.post('/completed/:id', async (req, res) => {
+			const id = req.params.id
+			const data = req.body
+			const result = await completeCollection.insertOne(data)
+			res.send(result)
+		})
+
+		app.get('/myTodo/:email', async (req, res) => {
+			const email = req.params.email
+			const query = { user: email }
+			const result = await (
+				await completeCollection.find(query).toArray()
+			).reverse()
+			res.send(result)
+		})
+
+		// adding user in the db
 		app.put('/user/:email', async (req, res) => {
 			const email = req.params.email
 			const user = req.body
